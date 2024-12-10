f93cc9916fb4d88f61d74efda2236cd161fc088f
abisha567 <a.adhikari.568@westcliff.edu>
2024-12-10 08:30:01 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import Nav from "./Component/Nav";
+import HeroSection from "./Component/Herosection";
+import Services from "./Component/Services";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <Herosection />
+      <Services />
+    </div>
+  );
+};
+
+export default App;
